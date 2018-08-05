54fc9e714969a69fa81399c3b8e59bf366115ac0
Wagner Santos <wag.097@gmail.com>
2018-08-05 17:11:41 -0300

    teste

---
+++ Ponto.hpp
@@ -0,0 +1,18 @@
+#ifndef PONTO_HPP
+#define PONTO_HPP
+
+class Ponto{
+
+private:
+	double x;
+	double y;
+	double z;
+
+public:
+	Ponto(double x, double y, double z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+}
