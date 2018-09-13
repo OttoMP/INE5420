6d17ff7fb72c1e637721669d1f5ce11e228839d3
Wagner Braga dos Santos <wag.097@gmail.com>
2018-09-13 08:48:43 -0300

    Update ponto.cpp

--- ponto.cpp
+++ ponto.cpp
@@ -6,6 +6,7 @@ Ponto::Ponto(double x, double y, double z)
 	this->coord[0] = x;
 	this->coord[1] = y;
 	this->coord[2] = z;
+	this->coord[3] = 1
 }
 
 Ponto::Ponto(double x, double y)
@@ -13,6 +14,7 @@ Ponto::Ponto(double x, double y)
 	this->coord[0] = x;
 	this->coord[1] = y;
 	this->coord[2] = 1;
+	this->coord[3] = 1;
 }
 
 double Ponto::get_x() const
@@ -34,7 +36,7 @@ bool Ponto::operator==(const Ponto& a) {
     auto epsilon = std::numeric_limits<double>::epsilon();
     return (std::fabs(this->coord[0] - a.get_x()) <= epsilon) &&
            (std::fabs(this->coord[1] - a.get_y()) <= epsilon) &&
-           (std::fabs(this->coord[2] - a.get_z()) <= epsilon);
+           (std::fabs(this->coord[2] - a.get_z()) <= epsilon) &&
 }	 	  	 	    	 	    		    	    	  	 	
 
 bool Ponto::operator!=(const Ponto& a) {
